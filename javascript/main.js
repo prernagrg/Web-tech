a8799ae262d93c62320ef134a10b274a2ade1bef
prernagrg <grgprena2023@gmail.com>
2025-11-18 11:29:19 +0545

    added function declaratiob types

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,51 @@
-function hello(){
-    alert("HI MY Name IS Prerana Gurung")
+// function hello(){
+//     alert("HI MY Name IS Prerana Gurung")
+// }
+
+// let a = 5;
+// let b = "5";
+// console.log(a==b);
+// console.log(a===b);
+
+console.log (greet("ram"));
+
+function greet(name){
+    return "Hello, "+name +"!"
 }
 
-let a = 5;
-let b = "5";
-console.log(a==b);
-console.log(a===b);
+console.log(greet("shyam"));
+
+
+const greet2 = function(name){
+    return "Hello," +name+ "!"
+}
+
+console.log(greet2("ram"));
+
+//arrow function
+
+const greet3 = (name)=>{
+    return "hello," +name+ ""
+}
+
+console.log(greet3("Barsha"));
+
+const greet4 = name =>"hellow " + name+ ""
+console.log(greet4("Shriya"));
+
+ function sum(...num){
+    let total = 0;
+    for (let i of num){
+        total += i;
+    }
+    return total;
+}
+console.log(sum(1, 2, 3, 4, 5, 5))
+
+let user = {
+    name: "Ram",
+    age: 23
+}
+const{name} = user
+// console.log(user.name)
+// console.log(user.age)

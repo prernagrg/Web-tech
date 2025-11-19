850d10502824c2bec0436df5c339476207e29f65
prernagrg <grgprena2023@gmail.com>
2025-11-19 11:14:21 +0545

    added some events in JS

--- javascript/main.js
+++ javascript/main.js
@@ -1,51 +1,64 @@
-// function hello(){
-//     alert("HI MY Name IS Prerana Gurung")
+// // function hello(){
+// //     alert("HI MY Name IS Prerana Gurung")
+// // }
+
+// // let a = 5;
+// // let b = "5";
+// // console.log(a==b);
+// // console.log(a===b);
+
+// console.log (greet("ram"));
+
+// function greet(name){
+//     return "Hello, "+name +"!"
 // }
 
-// let a = 5;
-// let b = "5";
-// console.log(a==b);
-// console.log(a===b);
-
-console.log (greet("ram"));
-
-function greet(name){
-    return "Hello, "+name +"!"
-}
-
-console.log(greet("shyam"));
+// console.log(greet("shyam"));
 
 
-const greet2 = function(name){
-    return "Hello," +name+ "!"
-}
+// const greet2 = function(name){
+//     return "Hello," +name+ "!"
+// }
 
-console.log(greet2("ram"));
+// console.log(greet2("ram"));
 
-//arrow function
+// //arrow function
 
-const greet3 = (name)=>{
-    return "hello," +name+ ""
-}
+// const greet3 = (name)=>{
+//     return "hello," +name+ ""
+// }
 
-console.log(greet3("Barsha"));
+// console.log(greet3("Barsha"));
 
-const greet4 = name =>"hellow " + name+ ""
-console.log(greet4("Shriya"));
+// const greet4 = name =>"hellow " + name+ ""
+// console.log(greet4("Shriya"));
 
- function sum(...num){
-    let total = 0;
-    for (let i of num){
-        total += i;
-    }
-    return total;
-}
-console.log(sum(1, 2, 3, 4, 5, 5))
+//  function sum(...num){
+//     let total = 0;
+//     for (let i of num){
+//         total += i;
+//     }
+//     return total;
+// }
+// console.log(sum(1, 2, 3, 4, 5, 5))
 
-let user = {
-    name: "Ram",
-    age: 23
-}
-const{name} = user
-// console.log(user.name)
-// console.log(user.age)
+// let user = {
+//     name: "Ram",
+//     age: 23
+// }
+// const{name} = user
+// // console.log(user.name)
+// // console.log(user.age)
+
+// const btn = document.querySelector("btn1");
+
+// btn.addEventListener('click', function(){
+//     alert('Button Clicked')
+// }
+
+const button = document.querySelectorAll(".multiplebtn");
+
+button.innerHTML = "Hello world"
+button.addEventListener('click', function(){
+    alert("this is a button");
+})
